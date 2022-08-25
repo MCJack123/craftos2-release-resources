2f44794b8f9170208d57987ca01ab2e9775059ee
MCJack123 <jackmacwindowslinux@gmail.com>
2022-08-25 13:47:17 -0400

    Fixed ROM upload script for Ubuntu

--- ubuntu/debian_craftos-pc-data/buildall.sh
+++ ubuntu/debian_craftos-pc-data/buildall.sh
@@ -1,19 +1,6 @@
 #!/bin/zsh
 find . -type f -not -path "./debian/*" -not -path "./.git*" -not -path ./README.md -not -path ./.pc | sed 's/^.\///g' | awk 'BEGIN {print "#!/usr/bin/env dh-exec"}; {print $1 " => /usr/share/craftos/" $1}' > debian/install
-RELEASES=(bionic focal jammy)
-VERSION=`grep -o "[0-9\.~\+]*-${RELEASES[1]}[0-9][0-9]*" debian/changelog | head -n 1 | sed 's/-.*//'`
-DEBVER=`grep -o "[0-9\.~\+]*-${RELEASES[1]}[0-9][0-9]*" debian/changelog | head -n 1 | sed 's/^.*-[^0-9]*//'`
-echo Building and uploading CraftOS-PC ROM v`echo $VERSION | sed "s/${RELEASES[1]}//"`...
-for i in $(seq 1 $((${#RELEASES[@]}))); do
-    echo Building package for ${RELEASES[$i]}...
-    if [ $i -gt 1 ]; then
-        sed "s/${RELEASES[$i-1]}/${RELEASES[$i]}/g" debian/changelog > debian/changelog.tmp
-        mv debian/changelog.tmp debian/changelog
-        VERSION=`echo $VERSION | sed "s/${RELEASES[$i-1]}/${RELEASES[$i]}/"`
-    fi
-    debuild -S -d
-    dput -U ppa:jackmacwindows/ppa ../craftos-pc-data_${VERSION}-${RELEASES[$i]}${DEBVER}_source.changes
-done
-sed "s/${RELEASES[$((${#RELEASES[@]}))]}/${RELEASES[1]}/g" debian/changelog > debian/changelog.tmp
-mv debian/changelog.tmp debian/changelog
-echo Done!
+VERSION=`grep -o "[0-9\.~\+]*-[0-9][0-9]*" debian/changelog | head -n 1 | sed 's/-.*//'`
+DEBVER=`grep -o "[0-9\.~\+]*-[0-9][0-9]*" debian/changelog | head -n 1 | sed 's/^.*-[^0-9]*//'`
+debuild -S -d
+dput -U ppa:jackmacwindows/ppa ../craftos-pc-data_${VERSION}-${DEBVER}_source.changes
