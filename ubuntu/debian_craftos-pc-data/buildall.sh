b8f12dc5c9a610dd24024f726f99394935bab1a7
JackMacWindows <jackmacwindowslinux@gmail.com>
2022-08-25 21:57:03 -0400

    Fixed ROM build script again

--- ubuntu/debian_craftos-pc-data/buildall.sh
+++ ubuntu/debian_craftos-pc-data/buildall.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-find . -type f -not -path "./debian/*" -not -path "./.git*" -not -path ./README.md -not -path ./.pc | sed 's/^.\///g' | awk 'BEGIN {print "#!/usr/bin/env dh-exec"}; {print $1 " => /usr/share/craftos/" $1}' > debian/install
+find . -type f -not -path "./debian/*" -not -path "./.git*" -not -path ./README.md -not -path ./.pc | sed 's/^.\///g' | awk 'BEGIN {print "#!/usr/bin/env dh-exec"}; {print $1 " => debian/craftos-pc-data/usr/share/craftos/" $1}' > debian/install
 VERSION=`grep -o "[0-9\.~\+]*-[0-9][0-9]*" debian/changelog | head -n 1 | sed 's/-.*//'`
 DEBVER=`grep -o "[0-9\.~\+]*-[0-9][0-9]*" debian/changelog | head -n 1 | sed 's/^.*-[^0-9]*//'`
 debuild -S -d
