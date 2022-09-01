c5a437e8c4be0406bd740f3e5b4126709e1302ae
MCJack123 <jackmacwindowslinux@gmail.com>
2022-09-01 18:01:04 -0400

    Disabled version swap

--- ubuntu/debian_craftos-pc-data/buildall.sh
+++ ubuntu/debian_craftos-pc-data/buildall.sh
@@ -1,8 +1,7 @@
 #!/bin/zsh
 find . -type f -not -path "./debian/*" -not -path "./.git*" -not -path ./README.md -not -path ./.pc | sed 's/^.\///g' | awk 'BEGIN {print "#!/usr/bin/env dh-exec"}; {print $1 " => /usr/share/craftos/" $1}' > debian/craftos-pc-data.install
 chmod a+x debian/craftos-pc-data.install
-sed 's/2.7.1-1/2.7.1-4/' debian/changelog > debian/changelog.new
-mv debian/changelog.new debian/changelog
+#sed 's/2.7.1-1/2.7.1-4/' debian/changelog > debian/changelog.new && mv debian/changelog.new debian/changelog
 VERSION=`grep -o "[0-9\.~\+]*-[0-9][0-9]*" debian/changelog | head -n 1 | sed 's/-.*//'`
 DEBVER=`grep -o "[0-9\.~\+]*-[0-9][0-9]*" debian/changelog | head -n 1 | sed 's/^.*-[^0-9]*//'`
 debuild -S -d
