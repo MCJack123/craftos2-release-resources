bcd5860cb0335e113e3fd3456615716894678594
MCJack123 <jackmacwindowslinux@gmail.com>
2022-09-01 17:40:19 -0400

    Fixing CI?

--- ubuntu/debian_craftos-pc-data/buildall.sh
+++ ubuntu/debian_craftos-pc-data/buildall.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-find . -type f -not -path "./debian/*" -not -path "./.git*" -not -path ./README.md -not -path ./.pc | sed 's/^.\///g' | awk 'BEGIN {print "#!/usr/bin/env dh-exec"}; {print $1 " /usr/share/craftos/" => $1}' > debian/craftos-pc-data.install
+find . -type f -not -path "./debian/*" -not -path "./.git*" -not -path ./README.md -not -path ./.pc | sed 's/^.\///g' | awk 'BEGIN {print "#!/usr/bin/env dh-exec"}; {print $1 " /usr/share/craftos/" $1}' > debian/craftos-pc-data.install
 chmod a+x debian/craftos-pc-data.install
 sed 's/2.7.1-1/2.7.1-3/' debian/changelog > debian/changelog.new
 mv debian/changelog.new debian/changelog
