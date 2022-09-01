c78f11ce9dab18da007e0921f2b21bf2e11e4115
MCJack123 <jackmacwindowslinux@gmail.com>
2022-09-01 17:10:41 -0400

    Fixing CI?

--- ubuntu/debian_craftos-pc-data/buildall.sh
+++ ubuntu/debian_craftos-pc-data/buildall.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
-find . -type f -not -path "./debian/*" -not -path "./.git*" -not -path ./README.md -not -path ./.pc | sed 's/^.\///g' | awk 'BEGIN {print "#!/usr/bin/env dh-exec"}; {print $1 " => /usr/share/craftos/" $1}' > debian/install
+find . -type f -not -path "./debian/*" -not -path "./.git*" -not -path ./README.md -not -path ./.pc | sed 's/^.\///g' | awk 'BEGIN {print "#!/usr/bin/env dh-exec"}; {print $1 " => ./usr/share/craftos/" $1}' > debian/install
+sed 's/2.7.1-1/2.7.1-2/' debian/changelog > debian/changelog.new
+mv debian/changelog.new debian/changelog
 VERSION=`grep -o "[0-9\.~\+]*-[0-9][0-9]*" debian/changelog | head -n 1 | sed 's/-.*//'`
 DEBVER=`grep -o "[0-9\.~\+]*-[0-9][0-9]*" debian/changelog | head -n 1 | sed 's/^.*-[^0-9]*//'`
 debuild -S -d
